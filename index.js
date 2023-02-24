23455f33fa8144f462d9bdbac9b1f29dfed6db97
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 17:24:04 +0000

    Changed inquirer version & created empty jest unit test for Questions class

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const Questions = require("./lib/Questions")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +14,6 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const myQuestions = new Questions();
+
+console.log(myQuestions)
