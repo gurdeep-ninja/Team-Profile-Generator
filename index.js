c48ab81f73afac2cfed83bcb84950c7164159afc
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 22:45:42 +0000

    Finished writing functions to prompt user with questions & create the html file

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const employees = [];
 
-function createTeam(employees){
+function createTeam() {
 
     const teamQuestion = new Questions();
 
@@ -22,16 +23,73 @@ function createTeam(employees){
 
         // When the user finishes answering the questions, proceed with the response data returned 
         .then((response) => {
-            console.log(response)
+
+            const choice = response.choices;
+            // "Engineer"
+            // "Intern"
+
+            if (choice === 'Engineer') {
+                const engineerQuestions = new Questions('engineer');
+
+                inquirer
+                    // The prompt is taking an array of objects which has all the questions
+                    .prompt(engineerQuestions.getEmployeeQuestions())
+
+                    // When the user finishes answering the questions, proceed with the response data returned 
+                    .then((response) => {
+                        // Create a new Engineer object based on the Engineer class
+                        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+
+                        // Push the Engineer to the employees array
+                        employees.push(engineer);
+
+                        // Show the next prompt for adding additional employees
+                        createTeam();
+                    }
+                    );
+            } else if (choice === 'Intern') {
+                const internQuestions = new Questions('intern');
+
+                inquirer
+                    // The prompt is taking an array of objects which has all the questions
+                    .prompt(internQuestions.getEmployeeQuestions())
+
+                    // When the user finishes answering the questions, proceed with the response data returned 
+                    .then((response) => {
+                        // Create a new Intern object based on the Intern class
+                        const intern = new Intern(response.name, response.id, response.email, response.school);
+
+                        // Push the Intern to the employees array
+                        employees.push(intern);
+
+                        // Show the next prompt for adding additional employees
+                        createTeam();
+                    }
+                    );
+            } else {
+
+                const html = render(employees);
+                //console.log(html);
+                writeToFile(outputPath,html);
+            }
         }
         );
+}
 
+// function to write html file
+function writeToFile(file, data) {
 
+    // Ise the writeFile() method of the fs module to write the file with data
+    fs.writeFile(file, data, (error) =>
+
+        // Upon the call back (success or failure), display the result to the user
+        // An error is displayed if theres an issue
+        // If successful, the file name is displayed to the user
+        error ? console.error(error) : console.log(`${file} created successfully!`)
+    )
 }
 
 function init() {
-
-    const employees = [];
 
     const managerQuestions = new Questions('manager');
 
@@ -42,13 +100,13 @@ function init() {
         // When the user finishes answering the questions, proceed with the response data returned 
         .then((response) => {
             // Create a new manager object based on the Manager class
-            const manager = new Manager(response.name, response.id, response.email,response.officeNumber);
+            const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
 
             // Push the manager to the employees array
             employees.push(manager);
 
             // Show the next prompt for adding additional employees
-            createTeam(employees);
+            createTeam();
         }
         );
 }
