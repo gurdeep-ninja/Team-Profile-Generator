f80a46316ff51d117193e7cacb044062291d54b9
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 22:25:59 +0000

    Started adding main app functionallity

--- index.js
+++ index.js
@@ -12,24 +12,43 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+function createTeam(employees){
 
+    const teamQuestion = new Questions();
+
+    inquirer
+        // The prompt is taking an array of objects which has all the questions
+        .prompt(teamQuestion.getTeamQuestions())
+
+        // When the user finishes answering the questions, proceed with the response data returned 
+        .then((response) => {
+            console.log(response)
+        }
+        );
+
+
+}
 
 function init() {
 
-    const 
+    const employees = [];
 
     const managerQuestions = new Questions('manager');
 
     inquirer
         // The prompt is taking an array of objects which has all the questions
-        .prompt(managerQuestions)
+        .prompt(managerQuestions.getEmployeeQuestions())
 
         // When the user finishes answering the questions, proceed with the response data returned 
         .then((response) => {
-            const manager = new Manager(response.name, response.id, response.email,response.officeNumber)
-            console.log(manager)
+            // Create a new manager object based on the Manager class
+            const manager = new Manager(response.name, response.id, response.email,response.officeNumber);
 
+            // Push the manager to the employees array
+            employees.push(manager);
+
+            // Show the next prompt for adding additional employees
+            createTeam(employees);
         }
         );
 }
