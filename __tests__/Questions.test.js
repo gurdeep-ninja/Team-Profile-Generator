2463f644d93e4a2be8e11edbf48a3cae3a95ac92
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 17:38:41 +0000

    Added 2 basic tests for Questions class

--- __tests__/Questions.test.js
+++ __tests__/Questions.test.js
@@ -5,3 +5,35 @@ const Questions = require("../lib/Questions");
 //   const e = new Intern("Foo", 1, "test@test.com", testValue);
 //   expect(e.school).toBe(testValue);
 // });
+
+//getNameQuestion(type) {}
+
+test("Can instantiate Questions instance", () => {
+    const e = new Questions();
+    expect(typeof(e)).toBe("object");
+  });
+
+it("Can set employee type", () => {
+  const testValue = "manager";
+  const e = new Questions(testValue);
+  expect(e.employeeType).toBe(testValue);
+});
+
+
+// test("Can set school via constructor", () => {
+//     const testValue = "UCLA";
+//     const e = new Intern("Foo", 1, "test@test.com", testValue);
+//     expect(e.school).toBe(testValue);
+//   });
+
+
+
+// getIdQuestion() {}
+
+// getEmailQuestion() {}
+
+// getOfficeQuestion(){}
+
+// getGitHubQuestion(){}
+
+// getSchoolQuestion(){}
