26b4297f6dd8272a2bf76cbff97e03b68d1fbff0
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 21:50:22 +0000

    started to implment main app functionality and fixed an issue with role not being correctly assigned when new class was instantiated

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const myQuestions = new Questions();
 
-console.log(myQuestions)
+function init() {
+
+    const managerQuestions = new Questions('manager');
+
+    inquirer
+        // The prompt is taking an array of objects which has all the questions
+        .prompt(managerQuestions)
+
+        // When the user finishes answering the questions, proceed with the response data returned 
+        .then((response) => {
+            const manager = new Manager(response.name, response.id, response.email,response.officeNumber)
+            console.log(manager)
+
+        }
+        );
+}
+
+init()
