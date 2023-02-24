4a7f2f60d2425b9beb09a150ca1163c8d0138e07
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-24 20:46:50 +0000

    Finished tests for Questions and implemented functions in the Questions class

--- __tests__/Questions.test.js
+++ __tests__/Questions.test.js
@@ -22,7 +22,7 @@ it("Can set employee type", () => {
 it("Can get the question for Manager's name", () => {
     const testValue = "Enter manager's name:";
     const e = new Questions('manager');
-    const nameQuestion = e.getNameQuestion()
+    const nameQuestion = e.getNameQuestion();
     expect(nameQuestion.message).toBe(testValue);
 });
 
@@ -30,7 +30,7 @@ it("Can get the question for Manager's name", () => {
 it("Can get the question for Engineer's name", () => {
     const testValue = "Enter engineer's name:";
     const e = new Questions('engineer');
-    const nameQuestion = e.getNameQuestion()
+    const nameQuestion = e.getNameQuestion();
     expect(nameQuestion.message).toBe(testValue);
 });
 
@@ -38,9 +38,48 @@ it("Can get the question for Engineer's name", () => {
 it("Can get the question for Intern's name", () => {
     const testValue = "Enter intern's name:";
     const e = new Questions('intern');
-    const nameQuestion = e.getNameQuestion()
+    const nameQuestion = e.getNameQuestion();
     expect(nameQuestion.message).toBe(testValue);
 });
+
+it("Can get the id question", () => {
+    const testValue = {type : 'input',  message : 'Enter ID:', name : 'id'}
+    const e = new Questions();
+    expect(e.getIdQuestion()).toMatchObject(testValue);
+
+});
+
+it("Can get the email question", () => {
+  const testValue = {type : 'input',  message : 'Enter email address:', name : 'email'}
+  const e = new Questions();
+  expect(e.getEmailQuestion()).toMatchObject(testValue);
+
+});
+
+
+it("Can get the office number question", () => {
+  const testValue = {type : 'input',  message : 'Enter office number:', name : 'officeNumber'}
+  const e = new Questions();
+  expect(e.getOfficeNumber()).toMatchObject(testValue);
+
+});
+
+it("Can get the github username question", () => {
+  const testValue = {type : 'input',  message : 'Enter Github username:', name : 'github'}
+  const e = new Questions();
+  expect(e.getGitHubQuestion()).toMatchObject(testValue);
+
+});
+
+
+it("Can get the intern school question", () => {
+  const testValue = {type : 'input',  message : 'Enter School:', name : 'school'}
+  const e = new Questions();
+  expect(e.getSchoolQuestion()).toMatchObject(testValue);
+
+});
+
+
 
 
 // test("Can set school via constructor", () => {
