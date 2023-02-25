f51f87a29e6c5e24c72c5484fb47599cc371d341
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2023-02-25 09:14:52 +0000

    Refactored the index.js file with seperate functions to better organise the code

--- index.js
+++ index.js
@@ -25,67 +25,82 @@ function createTeam() {
         .then((response) => {
 
             const choice = response.choices;
-            // "Engineer"
-            // "Intern"
 
             if (choice === 'Engineer') {
-                const engineerQuestions = new Questions('engineer');
 
-                inquirer
-                    // The prompt is taking an array of objects which has all the questions
-                    .prompt(engineerQuestions.getEmployeeQuestions())
+                createEngineer();
 
-                    // When the user finishes answering the questions, proceed with the response data returned 
-                    .then((response) => {
-                        // Create a new Engineer object based on the Engineer class
-                        const engineer = new Engineer(response.name, response.id, response.email, response.github);
-
-                        // Push the Engineer to the employees array
-                        employees.push(engineer);
-
-                        // Show the next prompt for adding additional employees
-                        createTeam();
-                    }
-                    );
             } else if (choice === 'Intern') {
-                const internQuestions = new Questions('intern');
 
-                inquirer
-                    // The prompt is taking an array of objects which has all the questions
-                    .prompt(internQuestions.getEmployeeQuestions())
+                createIntern();
 
-                    // When the user finishes answering the questions, proceed with the response data returned 
-                    .then((response) => {
-                        // Create a new Intern object based on the Intern class
-                        const intern = new Intern(response.name, response.id, response.email, response.school);
-
-                        // Push the Intern to the employees array
-                        employees.push(intern);
-
-                        // Show the next prompt for adding additional employees
-                        createTeam();
-                    }
-                    );
             } else {
 
-                const html = render(employees);
-                //console.log(html);
-                writeToFile(outputPath,html);
+                createHtmlFile();
             }
         }
         );
 }
 
+// function to create a new engineer
+
+function createEngineer() {
+
+    const engineerQuestions = new Questions('engineer');
+
+    inquirer
+        // The prompt is taking an array of objects which has all the questions
+        .prompt(engineerQuestions.getEmployeeQuestions())
+
+        // When the user finishes answering the questions, proceed with the response data returned 
+        .then((response) => {
+            // Create a new Engineer object based on the Engineer class
+            const engineer = new Engineer(response.name, response.id, response.email, response.github);
+
+            // Push the Engineer to the employees array
+            employees.push(engineer);
+
+            // Show the next prompt for adding additional employees
+            createTeam();
+        }
+        );
+}
+
+// function to create a new intern
+
+function createIntern() {
+    const internQuestions = new Questions('intern');
+
+    inquirer
+        // The prompt is taking an array of objects which has all the questions
+        .prompt(internQuestions.getEmployeeQuestions())
+
+        // When the user finishes answering the questions, proceed with the response data returned 
+        .then((response) => {
+            // Create a new Intern object based on the Intern class
+            const intern = new Intern(response.name, response.id, response.email, response.school);
+
+            // Push the Intern to the employees array
+            employees.push(intern);
+
+            // Show the next prompt for adding additional employees
+            createTeam();
+        }
+        );
+}
+
 // function to write html file
-function writeToFile(file, data) {
+function createHtmlFile() {
+
+    const html = render(employees);
 
     // Ise the writeFile() method of the fs module to write the file with data
-    fs.writeFile(file, data, (error) =>
+    fs.writeFile(outputPath, html, (error) =>
 
         // Upon the call back (success or failure), display the result to the user
         // An error is displayed if theres an issue
         // If successful, the file name is displayed to the user
-        error ? console.error(error) : console.log(`${file} created successfully!`)
+        error ? console.error(error) : console.log(`${outputPath} created successfully!`)
     )
 }
 
